4b0ff3ed6a09a01f54dae863ff80c7e17d66f050
Fernando Blat <fernando@blat.es>
2021-11-24 06:49:00 +0100

    Configure Azure storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,10 +23,10 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  # 
+  #
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -36,7 +36,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :microsoft
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
