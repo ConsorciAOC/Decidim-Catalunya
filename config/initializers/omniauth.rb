cad3f23f4ae94e8cd1b0144be6c167c9b831dd26
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-12-01 17:12:43 +0100

    Ignore ignore Omniauth::AuthenticityTokenProtection in request validation phase also in staging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 OmniAuth.config.full_host = Rails.application.secrets.omniauth[:idcat_mobil][:full_host]
-OmniAuth.config.request_validation_phase = Rails.env.development? ? nil : OmniAuth::AuthenticityTokenProtection
+OmniAuth.config.request_validation_phase = Rails.env.production? ? nil : OmniAuth::AuthenticityTokenProtection
