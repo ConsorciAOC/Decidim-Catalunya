63d4f6e9c51f972d39cd48eaa4251edd6e9d34a7
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-12-28 20:20:40 +0100

    Allow GET requests in OmniAuth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,4 @@
 
 OmniAuth.config.full_host = Rails.application.secrets.omniauth[:idcat_mobil][:full_host]
 OmniAuth.config.request_validation_phase = nil
+OmniAuth.config.allowed_request_methods = [:post, :get]
