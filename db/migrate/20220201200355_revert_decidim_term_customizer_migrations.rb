d8fa2ef17024e9ccf7640092ed2f41ab0cf1910c
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-02-01 21:15:44 +0100

    Add migration to revert term customizer migrations

---
+++ db/migrate/20220201200355_revert_decidim_term_customizer_migrations.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class RevertDecidimTermCustomizerMigrations < ActiveRecord::Migration[6.0]
+  def change
+    drop_table :decidim_term_customizer_constraints do |t|
+      t.references :decidim_organization, null: false, foreign_key: true, index: { name: "decidim_term_customizer_constraint_organization" }
+      t.references :subject, polymorphic: true, index: { name: "decidim_term_customizer_constraint_subject" }
+
+      t.references(
+        :translation_set,
+        null: false,
+        foreign_key: { to_table: :decidim_term_customizer_translation_sets },
+        index: { name: "decidim_term_customizer_constraint_translation_set" }
+      )
+    end
+
+    drop_table :decidim_term_customizer_translations do |t|
+      t.string :locale
+      t.string :key
+      t.text :value
+
+      t.references(
+        :translation_set,
+        null: false,
+        foreign_key: { to_table: :decidim_term_customizer_translation_sets },
+        index: { name: "decidim_term_customizer_translation_translation_set" }
+      )
+    end
+
+    drop_table :decidim_term_customizer_translation_sets do |t|
+      t.jsonb :name
+    end
+  end
+end
