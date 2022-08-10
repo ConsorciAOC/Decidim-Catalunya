fccfc8433ea9a1f316590e343c7013057a9d1733
Laura Jaime <laura.jv@coditramuntana.com>
2022-08-10 08:25:29 +0200

    Changes in XML

--- decidim-via_oberta_authorization/app/services/via_oberta_authorization_rq.rb
+++ decidim-via_oberta_authorization/app/services/via_oberta_authorization_rq.rb
@@ -33,61 +33,72 @@ class ViaObertaAuthorizationRq
       http_request.headers["Content-Type"] = "text/xml"
       http_request.headers["SOAPAction"] = "servicio"
       http_request.body = request_body(request)
+      puts http_request.body
     end
   end
 
   def request_body(request)
     timestamp = Time.now.strftime("%Y-%m-%dT%H:%M:%SZ")
     <<~XML
-    <?xml version="1.0" encoding="UTF-8"?>
-    <Peticion xmlns="http://gencat.net/scsp/esquemes/peticion">
-      <Atributos>
-        <IdPeticion>DECIDIM-PAD-#{@ine}-#{timestamp}</IdPeticion>
-        <NumElementos>1</NumElementos>
-        <TimeStamp>#{timestamp}</TimeStamp>
-        <CodigoCertificado>RESIDENT_MUNICIPI</CodigoCertificado>
-        <CodigoProducto>PADRO</CodigoProducto>
-        <DatosAutorizacion>
-          <IdentificadorSolicitante>#{@ine}</IdentificadorSolicitante>
-          <NombreSolicitante>Ajuntament de Tarragona</NombreSolicitante>
-          <Finalidad>#{ViaObertaAuthorizationConfig.purpose}</Finalidad>
-        </DatosAutorizacion>
-        <Emisor>
-          <NifEmisor>Q0801175A</NifEmisor>
-          <NombreEmisor>CAOC</NombreEmisor>
-        </Emisor>
-      </Atributos>
-      <Solicitudes>
-        <SolicitudTransmision>
-          <DatosGenericos>
-            <Emisor>
-              <NifEmisor>Q0801175A</NifEmisor>
-              <NombreEmisor>CAOC</NombreEmisor>
-            </Emisor>
-            <Solicitante>
-              <IdentificadorSolicitante>#{@ine}</IdentificadorSolicitante>
-              <NombreSolicitante>CAOC</NombreSolicitante>
-              <Finalidad>Ajuntament de Tarragona</Finalidad>
-              <Consentimiento>Si</Consentimiento>
-            </Solicitante>
-            <Transmision>
-              <CodigoCertificado>RESIDENT_MUNICIPI</CodigoCertificado>
-              <IdSolicitud>AOC00000000100373</IdSolicitud>
-            </Transmision>
-          </DatosGenericos>
-          <DatosEspecificos>
-            <ns1:peticionResidente xmlns:ns1="http://www.aocat.net/padro">
-              <ns1:numExpediente>#{request.id_document}</ns1:numExpediente>
-              <ns1:tipoDocumentacion>#{request.document_type}</ns1:tipoDocumentacion>
-              <ns1:documentacion>#{request.id_document}</ns1:documentacion>
-              <ns1:codigoMunicipio>#{@municipal_code}</ns1:codigoMunicipio> 
-							<ns1:codigoProvincia>#{@province_code}</ns1:codigoProvincia>
-              <ns1:idescat>1</ns1:idescat>
-            </ns1:peticionResidente>
-          </DatosEspecificos>
-        </SolicitudTransmision>
-      </Solicitudes>
-    </Peticion>
+      <?xml version="1.0" encoding="UTF-8"?>
+      <env:Envelope
+          xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xmlns:impl="http://gencat.net/scsp/esquemes/peticion"
+          xmlns:env="http://schemas.xmlsoap.org/soap/envelope/">
+        <env:Body>
+          <open:procesa>
+            <Peticion xmlns="http://gencat.net/scsp/esquemes/peticion">
+              <Atributos>
+                <IdPeticion>DECIDIM-PAD-#{@ine}-#{timestamp}</IdPeticion>
+                <NumElementos>1</NumElementos>
+                <TimeStamp>#{timestamp}</TimeStamp>
+                <CodigoCertificado>RESIDENT_MUNICIPI</CodigoCertificado>
+                <CodigoProducto>PADRO</CodigoProducto>
+                <DatosAutorizacion>
+                  <IdentificadorSolicitante>#{@ine}</IdentificadorSolicitante>
+                  <NombreSolicitante>Ajuntament de Tarragona</NombreSolicitante>
+                  <Finalidad>#{ViaObertaAuthorizationConfig.purpose}</Finalidad>
+                </DatosAutorizacion>
+                <Emisor>
+                  <NifEmisor>Q0801175A</NifEmisor>
+                  <NombreEmisor>CAOC</NombreEmisor>
+                </Emisor>
+              </Atributos>
+              <Solicitudes>
+                <SolicitudTransmision>
+                  <DatosGenericos>
+                    <Emisor>
+                      <NifEmisor>Q0801175A</NifEmisor>
+                      <NombreEmisor>CAOC</NombreEmisor>
+                    </Emisor>
+                    <Solicitante>
+                      <IdentificadorSolicitante>#{@ine}</IdentificadorSolicitante>
+                      <NombreSolicitante>CAOC</NombreSolicitante>
+                      <Finalidad>Ajuntament de Tarragona</Finalidad>
+                      <Consentimiento>Si</Consentimiento>
+                    </Solicitante>
+                    <Transmision>
+                      <CodigoCertificado>RESIDENT_MUNICIPI</CodigoCertificado>
+                      <IdSolicitud>AOC00000000100373</IdSolicitud>
+                    </Transmision>
+                  </DatosGenericos>
+                  <DatosEspecificos>
+                    <ns1:peticionResidente xmlns:ns1="http://www.aocat.net/padro">
+                      <ns1:numExpediente>#{request.id_document}</ns1:numExpediente>
+                      <ns1:tipoDocumentacion>#{request.document_type}</ns1:tipoDocumentacion>
+                      <ns1:documentacion>#{request.id_document}</ns1:documentacion>
+                      <ns1:codigoMunicipio>#{@municipal_code}</ns1:codigoMunicipio> 
+                      <ns1:codigoProvincia>#{@province_code}</ns1:codigoProvincia>
+                      <ns1:idescat>1</ns1:idescat>
+                    </ns1:peticionResidente>
+                  </DatosEspecificos>
+                </SolicitudTransmision>
+              </Solicitudes>
+            </Peticion>
+          </open:procesa>
+        </env:Body>
+      </env:Envelope>
     XML
   end
 
