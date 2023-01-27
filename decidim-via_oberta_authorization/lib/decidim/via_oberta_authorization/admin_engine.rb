48ad595d7c0517a6ce8c90ebb8eee1c5e2523ed1
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-01-27 15:40:42 +0100

    Correct readme and fix engine (#18)
    * FIX: Add nil paths
    
    To avoid double execution of tasks and migrations
    
    * DOC: correct testing documentation

--- decidim-via_oberta_authorization/lib/decidim/via_oberta_authorization/admin_engine.rb
+++ decidim-via_oberta_authorization/lib/decidim/via_oberta_authorization/admin_engine.rb
@@ -4,6 +4,9 @@ module Decidim
   module ViaObertaAuthorization
     class AdminEngine < ::Rails::Engine
       isolate_namespace Decidim::ViaObertaAuthorization::Admin
+
+      paths["db/migrate"] = nil
+      paths["lib/tasks"] = nil
     end
   end
 end
