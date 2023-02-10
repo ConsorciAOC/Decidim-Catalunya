a476e335d6dc25ffdcac534ef88760fb85f075bd
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-02-10 13:30:33 +0100

    FIX: take into account on create

--- app/decorators/decidim/system/update_organization_form_decorator.rb
+++ app/decorators/decidim/system/update_organization_form_decorator.rb
@@ -7,12 +7,12 @@ module Decidim
         Decidim::System::UpdateOrganizationForm.class_eval do
           jsonb_attribute :google_tag_manager_settings, [
             [:entity_code, String],
-            [:entity_name, String],
+            [:entity_name, String]
           ]
 
           def self.from_model(organization)
-            form= super
-            form.google_tag_manager_settings["entity_code"]= organization.via_oberta_settings["ine"] if form.google_tag_manager_settings
+            form = super
+            form.google_tag_manager_settings["entity_code"] = organization.via_oberta_settings["ine"] if form.google_tag_manager_settings
             form
           end
         end
