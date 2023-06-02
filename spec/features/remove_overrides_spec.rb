3fde1dd825490731f87e9b38ea5c8a21d5ecc17c
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-06-02 13:07:40 +0200

    Update spec/features/remove_overrides_spec.rb

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 
 RSpec.describe "Overrides" do
   it "remove overrides after upgrading to v0.26" do
-    # - Check header and application overrides views to set Google Tag Manager
+    # - Check header and application overrides views to set Google Tag Manager (see #22 and "Add Google Tag Manager to admin backoffice" in the README 
     expect(Decidim.version).to be < "0.26"
   end
 end
