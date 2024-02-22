553d3ebf51a6c5625e5a94901b74e55e38ed1436
Laura Jaime <laura.jv@coditramuntana.com>
2024-02-22 09:05:57 +0100

    Apply release notes

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -2,22 +2,24 @@
 
 Decidim.configure do |config|
   # The name of the application
-  config.application_name = "Decidim Catalunya"
+  config.application_name = Rails.application.secrets.decidim[:application_name]
 
   # The email that will be used as sender in all emails from Decidim
-  config.mailer_sender = Rails.env.production? ? "no-reply@decidimcatalunya.cat" : "no-reply-pre@decidimcatalunya.cat"
+  config.mailer_sender = Rails.application.secrets.decidim[:mailer_sender]
 
   # Sets the list of available locales for the whole application.
   #
   # When an organization is created through the System area, system admins will
   # be able to choose the available languages for that organization. That list
   # of languages will be equal or a subset of the list in this file.
+  # config.available_locales = Rails.application.secrets.decidim[:available_locales].presence || [:en]
+  # Or block set it up manually and prevent ENV manipulation:
   config.available_locales = [:ca, :es]
 
   # Sets the default locale for new organizations. When creating a new
   # organization from the System area, system admins will be able to overwrite
   # this value for that specific organization.
-  config.default_locale = Rails.env.test? ? :en : :ca
+  config.default_locale = Rails.application.secrets.decidim[:default_locale].presence || :en
 
   # Map and Geocoder configuration
   config.maps = {
@@ -39,12 +41,23 @@ Decidim.configure do |config|
   # that an organization's administrator injects malicious scripts to spy on or
   # take over user accounts.
   #
-  config.enable_html_header_snippets = true
+  config.enable_html_header_snippets = Rails.application.secrets.decidim[:enable_html_header_snippets].present?
+
+  # Allow organizations admins to track newsletter links.
+  config.track_newsletter_links = Rails.application.secrets.decidim[:track_newsletter_links].present? unless Rails.application.secrets.decidim[:track_newsletter_links] == "auto"
 
   # Machine Translation Configuration
   #
   # Enable machine translations
   config.enable_machine_translations = false
+
+  # Max requests in a time period to prevent DoS attacks. Only applied on production.
+  config.throttling_max_requests = Rails.application.secrets.decidim[:throttling_max_requests].to_i
+
+  # Time window in which the throttling is applied.
+  config.throttling_period = Rails.application.secrets.decidim[:throttling_period].to_i.minutes
+
+  config.follow_http_x_forwarded_host = Rails.application.secrets.decidim[:follow_http_x_forwarded_host].present?
 end
 
 Rails.application.config.i18n.available_locales = Decidim.available_locales
