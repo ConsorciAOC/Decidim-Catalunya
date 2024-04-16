e79f68d07efaef2c788877747f1e35ddd492cc9d
Laura Jaime <laura.jv@coditramuntana.com>
2024-04-16 17:29:31 +0200

    Add missing translations of surveys

--- spec/features/remove_overrides_spec.rb
+++ spec/features/remove_overrides_spec.rb
@@ -5,6 +5,7 @@ require "spec_helper"
 RSpec.describe "Overrides" do
   it "remove overrides after upgrading to v0.28" do
     # - Check header and application overrides views to set Google Tag Manager (see #22 and "Add Google Tag Manager to admin backoffice" in the README)
+    # - Check survey translations
     expect(Decidim.version).to be < "0.28"
   end
 end
