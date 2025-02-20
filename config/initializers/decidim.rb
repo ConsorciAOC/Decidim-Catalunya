c21b13c7246599cdb58ccd298808b8a515a8b7d8
Laura Jaime <laura.jv@coditramuntana.com>
2025-02-20 15:22:33 +0100

    Configure CSP

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -30,6 +30,12 @@ Decidim.configure do |config|
     units: :km
   }
 
+  # Configure CSP for Azure Storage
+  config.content_security_policies_extra = {
+    "connect-src" =>  %W("#{ENV["AZURE_STORAGE_URL"]} https://www.googletagmanager.com"),
+    "img-src" => %W("#{ENV["AZURE_STORAGE_URL"]} https://www.googletagmanager.com")
+  }
+
   # Custom HTML Header snippets
   #
   # The most common use is to integrate third-party services that require some
