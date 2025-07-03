be137105b8c52498ee441e8473f1a2be37f286f9
Laura Jaime <laura.jv@coditramuntana.com>
2025-07-03 16:42:12 +0200

    Rework GTM overrides

--- app/overrides/layouts/decidim/_wrapper.rb
+++ app/overrides/layouts/decidim/_wrapper.rb
@@ -3,11 +3,5 @@
 Deface::Override.new(virtual_path: "layouts/decidim/_wrapper",
                      name: "add_google_tag_manager_to_body",
                      insert_before: ".layout-container",
-                     original: "e9d156be0184f0a4576b5c7790a8c0e2d2e2ac73") do
-  <<-EOGTM
-  <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<%= google_tag_manager_code %>"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->
-  EOGTM
-end
+                     original: "e9d156be0184f0a4576b5c7790a8c0e2d2e2ac73",
+                     text: "<%= render partial: 'layouts/decidim/google_tag_manager' %>")
