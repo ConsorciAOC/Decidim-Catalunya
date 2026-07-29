e6c311ac0c28486333d4e4320c45d6924d426cce
Ivan Vergés <ivan@pokecode.net>
2026-07-29 10:52:01 +0200

    fix migration order

--- db/migrate/20260626102338_add_answer_options_counter_cache_to_questions.decidim_forms.rb
+++ db/migrate/20260626102338_add_answer_options_counter_cache_to_questions.decidim_forms.rb
@@ -2,13 +2,21 @@
 
 # This migration comes from decidim_forms (originally 20240402092039)
 class AddAnswerOptionsCounterCacheToQuestions < ActiveRecord::Migration[6.1]
+  class Question < ActiveRecord::Base
+    self.table_name = "decidim_forms_questions"
+    has_many :answer_options, class_name: "AnswerOption", foreign_key: "decidim_question_id"
+  end
+  class AnswerOption < ActiveRecord::Base
+    self.table_name = "decidim_forms_answer_options"
+    belongs_to :question, class_name: "Question", foreign_key: "decidim_question_id", counter_cache: :answer_options_count
+  end
   def change
     add_column :decidim_forms_questions, :answer_options_count, :integer, null: false, default: 0
 
     reversible do |dir|
       dir.up do
-        Decidim::Forms::Question.reset_column_information
-        Decidim::Forms::Question.find_each do |record|
+        Question.reset_column_information
+        Question.find_each do |record|
           record.class.reset_counters(record.id, :answer_options)
         end
       end
